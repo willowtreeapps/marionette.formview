a7b40296e00e2c0913e39162a63f1b1497fe1c3b
Tucker Legard <tlegard@vt.edu>
2014-08-27 13:07:45 -0400

    Throw error if validator does not exist

--- src/marionette.formview.js
+++ src/marionette.formview.js
@@ -240,10 +240,11 @@
 
       if (this.rules && this.rules[validationRule]) {
         return _(this.rules[validationRule]).bind(this)(val);
+      } else if (_.isFunction(this.formValidator[validationRule])) {
+        return _(this.formValidator[validationRule]).bind(this)(val,options);
       } else {
-        if (_.isFunction(this.formValidator[validationRule])) {
-          return _(this.formValidator[validationRule]).bind(this)(val,options);
-        }
+        throw new Error('Validator does not exist : ' + validator);
+        return false;
       }
 
       return true;
@@ -278,11 +279,6 @@
           email : /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
           alpha : /^[a-zA-Z]+$/,
           alphanum : /^[a-zA-Z0-9]+$/
-        },
-
-        validate : function(validator, val, options) {
-          if (_.isFunction(this[validator])) return _(this[validator]).bind(this)(val,options);
-          throw new Error('Validator does not exist : ' + validator);
         },
 
         matches : function(val,field) {
