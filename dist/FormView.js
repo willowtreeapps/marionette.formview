7947c38fcc3708c300845d90354f148f8db3fb50
Tucker Legard <tlegard@vt.edu>
2014-08-27 11:02:36 -0400

    Bump version.

--- dist/FormView.js
+++ dist/FormView.js
@@ -1,4 +1,4 @@
-/*! marionette-formview - v1.0.2 - 2014-06-29 */
+/*! marionette-formview - v1.0.3 - 2014-08-27 */
 /*global Backbone,define*/
 
 ;(function (root, factory) {
@@ -232,7 +232,7 @@
       // throw an error because it could be tough to troubleshoot if we just return false
       if (!validationRule) throw new Error('Not passed a validation to test');
 
-      if (validationRule === 'required') return FormValidator.required(val);
+      if (validationRule === 'required') return this.formValidator.required(val);
 
       if (validationRule.indexOf(':') !== -1) {
         options = validationRule.split(":");
@@ -241,9 +241,13 @@
 
       if (this.rules && this.rules[validationRule]) {
         return _(this.rules[validationRule]).bind(this)(val);
+      } else if (_.isFunction(this.formValidator[validationRule])) {
+        return _(this.formValidator[validationRule]).bind(this)(val,options);
       } else {
-        return _(FormValidator.validate).bind(this)(validationRule, val, options);
+        throw new Error('Validator does not exist : ' + validator);
+        return false;
       }
+
       return true;
     },
 
@@ -268,63 +272,57 @@
       this.populateFields();
       this.bindFormEvents();
       if (_.isFunction(this.onReady)) this.onReady();
-    }
+    },
+
+    formValidator : {
+        regex : {
+          //RFC 2822
+          email : /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
+          alpha : /^[a-zA-Z]+$/,
+          alphanum : /^[a-zA-Z0-9]+$/
+        },
+
+        matches : function(val,field) {
+          /*jshint eqeqeq:false*/
+          return val == this.inputVal(field);
+        },
+
+        min : function(val,minLength) {
+          if (val.length < minLength) return false;
+          return true;
+        },
+
+        max : function(val, maxLength) {
+          if (val.length > maxLength) return false;
+          return true;
+        },
+
+        numeric : function(val) {
+          return _.isNumber(val);
+        },
+
+        alpha : function(val) {
+          return this.formValdator.regex.alpha.test(val);
+        },
+
+        alphanum : function (val) {
+          return this.formValidator.regex.alphanum.test(val);
+        },
+
+        email :function(val) {
+            return this.formValidator.regex.email.test(val);
+        },
+
+        required : function(val) {
+          if (val === false || _.isNull(val) || _.isUndefined(val) ||  (_.isString(val) && val.length === 0)) return false;
+          return true;
+        },
+
+        boolean : function(val) {
+          return _.isBoolean(val);
+        }
+      }
   });
-
-  var FormValidator = {
-
-    regex : {
-      //RFC 2822
-      email : /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
-      alpha : /^[a-zA-Z]+$/,
-      alphanum : /^[a-zA-Z0-9]+$/
-    },
-
-    validate : function(validator, val, options) {
-      if (_.isFunction(FormValidator[validator])) return _(FormValidator[validator]).bind(this)(val,options);
-      throw new Error('Validator does not exist : ' + validator);
-    },
-
-    matches : function(val,field) {
-      /*jshint eqeqeq:false*/
-      return val == this.inputVal(field);
-    },
-
-    min : function(val,minLength) {
-      if (val.length < minLength) return false;
-      return true;
-    },
-
-    max : function(val, maxLength) {
-      if (val.length > maxLength) return false;
-      return true;
-    },
-
-    numeric : function(val) {
-      return _.isNumber(val);
-    },
-
-    alpha : function(val) {
-      return FormValidator.regex.alpha.test(val);
-    },
-
-    alphanum : function (val) {
-      return FormValidator.regex.alphanum.test(val);
-    },
-
-    email : function(val) {
-      return FormValidator.regex.email.test(val);
-    },
-
-    required : function(val) {
-      if (val === false || _.isNull(val) || _.isUndefined(val) ||  (_.isString(val) && val.length === 0)) return false;
-      return true;
-    },
-
-    boolean : function(val) {
-      return _.isBoolean(val);
-    }
-  };
 
   return FormView;
 }));
